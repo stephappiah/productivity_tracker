021dbc24354cc9e24a26c5d5265e0b1ba4cb6c76
Stephen Appiah <appstephen8@gmail.com>
2021-07-20 23:26:02 +0000

    add todo & delete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,79 @@
-import React, { Component } from 'react'
-import Todos from './components/Todos'
+import React, { Component } from "react";
+import Todos from "./components/Todos";
 
-import './App.css'
+import "./App.css";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import {v4 as uuid} from 'uuid'
+
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid(),
+        title: "Write something",
+        completed: false,
+      },
+      {
+        id: uuid(),
+        title: "Another bullshitting",
+        completed: false,
+      },
+      {
+        id: uuid(),
+        title: "Meeting with boss",
+        completed: false,
+      },
+    ],
+  };
+
+  markComplete = (id) => {
+    // set state on completed to the oposite
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+
+        return todo;
+      }),
+    });
+  };
+
+  deleteItem = (id) => {
+    this.setState({
+      // just filter the state to exclude this id
+      todos: [...this.state.todos.filter((todo) => todo.id !== id)],
+    });
+  };
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title: title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
   render() {
+    console.log(this.state.todos);
+
     return (
-      <div>
-          <Todos/>
+      <div className='App'>
+        <div className='container'>
+            <Header />
+            <AddTodo addTodo={this.addTodo}/>
+            <Todos
+              todos={this.state.todos}
+              markComplete={this.markComplete}
+              deleteItem={this.deleteItem}
+            />
+        </div>
+        
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
 
 class Todos extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Todos</h1>
-            </div>
-        )
-    }
+  render() {
+    return this.props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+        deleteItem={this.props.deleteItem}
+      />
+    ));
+  }
 }
 
-export default Todos
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired,
+};
+
+export default Todos;
